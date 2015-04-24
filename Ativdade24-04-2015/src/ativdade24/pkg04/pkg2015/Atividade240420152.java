5920c6a565ec485646d5a27ccb76d584f2664285
12122100156 <12122100156@15lab115.umc.br>
2015-04-24 20:14:26 -0300

    Submetendo arquivo novo no repositório local

---
+++ Ativdade24-04-2015/src/ativdade24/pkg04/pkg2015/Atividade240420152.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ativdade24.pkg04.pkg2015;
+
+/**
+ *
+ * @author 12122100156
+ */
+public class Atividade240420152 {
+    
+}
